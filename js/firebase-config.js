f54fea5fa6e8e406c83f35a813a801ceaacd0194
Pawel <klawinski.pawel@gmail.com>
2025-12-17 13:57:52 +0100

    Add Firebase configuration file

---
+++ js/firebase-config.js
@@ -0,0 +1,9 @@
+// js/firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyDkT6b71bvy_AC3A3p8tBzQPe6vnPcX3bQ",
+  authDomain: "erj---teczka-44ee7.firebaseapp.com",
+  projectId: "erj---teczka-44ee7",
+  storageBucket: "erj---teczka-44ee7.appspot.com",
+  messagingSenderId: "331154622390",
+  appId: "1:331154622390:web:2fc6982723def7ab1f6317"
+};
