af68dde711ea08ba0fa4616a08c67727e376767d
Pawel <klawinski.pawel@gmail.com>
2025-12-17 15:24:36 +0100

    Create firebase-config.js

---
+++ public/firebase-config.js
@@ -0,0 +1,9 @@
+// firebase-config.js
+import { initializeApp } from "firebase/app";
+export const firebaseConfig = {
+  apiKey: "XXX",
+  authDomain: "erj---teczka-44ee7.firebaseapp.com",
+  projectId: "erj---teczka-44ee7",
+  // ...
+};
+export const app = initializeApp(firebaseConfig);
